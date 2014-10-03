0acdfddc572b28e4ad7877619bee97783582e98d
Cory Rodriguez <coryrodriguez@Corys-MacBook-Air.local>
2014-10-03 14:53:50 -0500

    updated to use blade syntax

--- app/views/roll-dice.blade.php
+++ app/views/roll-dice.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.master')
 
 @section('content')
-    <p>Your guess is: <?= $guess ?></p>
+    <p>Your guess is: {{{$guess}}}</p>
 
-    <p>The random number is: <?= $randNum ?></p>
+    <p>The random number is: {{{$randNum}}}</p>
 
-    <?php if ($guess == $randNum): ?>
-    <p>You guessed correctly!</p>
-    <?php endif ?>
+    @if ($guess == $randNum)
+        <p>You guessed correctly!</p>
+    @endif
 @stop
