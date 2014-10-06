5de4f9e43d355e9a392ecedc4e404f41c0b13d9a
Cory Rodriguez <coryrodriguez@Corys-MacBook-Air.local>
2014-10-06 10:44:34 -0500

    added jquery and bootstrap to master blade template

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -1,16 +1,18 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Laravel Site</title>
-        <!-- inc bootstrap -->
-        <!-- inc jquery -->
-        <!-- inc other meta -->
-    </head>
-    <body>
-        <!-- heading -->
-        <!-- nav bar -->
-        @yield('content')
-        <!-- standard footer -->
-        <!-- js files -->
-    </body>
+<head>
+    <title>Laravel Site</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
+    <!-- inc jquery -->
+    <!-- inc other meta -->
+</head>
+<body>
+    <!-- heading -->
+    <!-- nav bar -->
+    @yield('content')
+    <!-- standard footer -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+</body>
 </html>
