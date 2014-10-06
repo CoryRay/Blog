1382fd4c4f1dcbe78851f078aecc20fb7c361e30
Cory Rodriguez <coryrodriguez@Corys-MacBook-Air.local>
2014-10-06 11:27:26 -0500

    changed to use a controller for resume and portfolio pages

--- app/routes.php
+++ app/routes.php
@@ -1,30 +1,13 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the Closure to execute when that URI is requested.
-|
-*/
-
 Route::get('/', function()
 {
     return View::make('hello');
 });
 
-Route::get('/resume', function()
-{
-    return View::make('resume');
-});
+Route::get('/resume', 'HomeController@showResume');
 
-Route::get('/portfolio', function()
-{
-    return View::make('portfolio');
-});
+Route::get('/portfolio', 'HomeController@showPortfolio');
 
 Route::get('/rolldice/{guess}', function($guess)
 {
