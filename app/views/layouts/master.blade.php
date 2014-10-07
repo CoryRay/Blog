9a78bee0a1139afad9799c741b99b4b63bd8aed0
Cory Rodriguez <coryrodriguez@Corys-MacBook-Air.local>
2014-10-07 14:07:40 -0500

    fixed comments

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -14,7 +14,6 @@
     <!-- Navigation -->
     <nav class="navbar navbar-inverse navbar-static-top" role="navigation">
         <div class="container">
-            <!-- Brand and toggle get grouped for better mobile display -->
             <div class="navbar-header">
                 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
                     <span class="sr-only">Toggle navigation</span>
@@ -22,47 +21,54 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="/">Cory's Blog</a>
+                <a class="navbar-brand" href="/posts">Cory's Blog</a>
             </div>
-            <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav">
                     @if ($_SERVER['REQUEST_URI'] == '/about')
 
                     <!-- route object laravel -->
-                        <li class='active'>
-                            <a href="/about">About</a>
-                        </li>
+                    <li class='active'>
+                        <a href="/about">About</a>
+                    </li>
                     @else
-                        <li>
-                            <a href="/about">About</a>
-                        </li>
+                    <li>
+                        <a href="/about">About</a>
+                    </li>
                     @endif
                     @if ($_SERVER['REQUEST_URI'] == '/resume')
-                        <li class='active'>
+                    <li class='active'>
                         <a href="/resume">Resume</a>
-                        </li>
+                    </li>
                     @else
-                        <li>
-                            <a href="/resume">Resume</a>
-                        </li>
+                    <li>
+                        <a href="/resume">Resume</a>
+                    </li>
                     @endif
                     @if ($_SERVER['REQUEST_URI'] == '/portfolio')
-                        <li class='active'>
+                    <li class='active'>
                         <a href="/portfolio">Portfolio</a>
-                        </li>
+                    </li>
                     @else
-                        <li>
-                            <a href="/portfolio">Portfolio</a>
-                        </li>
+                    <li>
+                        <a href="/portfolio">Portfolio</a>
+                    </li>
                     @endif
                 </ul>
             </div>
-            <!-- /.navbar-collapse -->
         </div>
-        <!-- /.container -->
     </nav>
     @yield('content')
-    <!-- standard footer -->
+
+    <div class='container'>
+        <hr>
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>Cory &copy; My Website 2014</p>
+                </div>
+            </div>
+        </footer>
+    </div>
 </body>
 </html>
