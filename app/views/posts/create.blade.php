9ce7a4f68e68b8691fe8ffc731270f3a9c3f6dae
Cory Rodriguez <coryrodriguez@Corys-MacBook-Air.local>
2014-10-08 13:26:54 -0500

    changed to use blade forms

--- app/views/posts/create.blade.php
+++ app/views/posts/create.blade.php
@@ -3,20 +3,20 @@
 @section('content')
 <div class='col-md-8'>
     <h2>Create a New Post</h2>
-    <form method='POST' action="{{{ action('PostsController@store') }}}">
-        <div class='form-group'>
-            <label for="title" >Post Title:</label>
-            <input id='title' class='form-control' name='title' type="text" value="{{{ Input::old('title') }}}" required>
-            {{ $errors->first('title', '<br><div class="alert alert-info">:message</div>') }}
-        </div>
-        <div class='form-group'>
-            <label for="body">Body:</label>
-            <textarea id="body" class='form-control' name="body" rows="5" required>{{{ Input::old('body') }}}</textarea>
-            {{ $errors->first('body', '<br><div class="alert alert-info">:message</div>') }}
-        </div>
-        <div class='form-group'>
-            <button class='btn btn-default'>Submit</button>
-        </div>
-    </form>
+    {{ Form::open(array('action' => 'PostsController@store')) }}
+    <div class='form-group'>
+        <label for="title" >Post Title:</label>
+        <input id='title' class='form-control' name='title' type="text" value="{{{ Input::old('title') }}}" required>
+        {{ $errors->first('title', '<br><div class="alert alert-info">:message</div>') }}
+    </div>
+    <div class='form-group'>
+        <label for="body">Body:</label>
+        <textarea id="body" class='form-control' name="body" rows="5" required>{{{ Input::old('body') }}}</textarea>
+        {{ $errors->first('body', '<br><div class="alert alert-info">:message</div>') }}
+    </div>
+    <div class='form-group'>
+        <button class='btn btn-default'>Submit</button>
+    </div>
+    {{ Form::close() }}
 </div>
 @stop
