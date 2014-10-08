0d87416dd728b8df52102eb4b35b233b75c2b98f
Cory Rodriguez <coryrodriguez@Corys-MacBook-Air.local>
2014-10-08 16:51:09 -0500

    fixed semantics

--- app/views/posts/index.blade.php
+++ app/views/posts/index.blade.php
@@ -8,20 +8,22 @@
     <a class='btn btn-default' href={{ action('PostsController@create') }}>New Post</a>
     <hr>
 
-<!-- Blog Post  -->
-    @forelse($posts as $post)
-    <h3 id='title'>{{{ $post->title }}}</h3>
-    <p id='date'>{{{ $post->updated_at->setTimezone('America/Chicago')->format(Post::DATE_FORMAT) }}}</p>
-    <img class='img-responsive' src="http://placehold.it/900x300" alt="">
-    <p id='body'>{{{ $post->body }}}</p>
-    <a class="btn btn-sm btn-primary" href="{{ $post->id }}">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
-    @empty
-    <p>No mo post</p>
-    @endforelse
+    <!-- Blog Post  -->
+    <article>
+        @forelse($posts as $post)
+        <h3 id='title'>{{{ $post->title }}}</h3>
+        <p id='date'>{{{ $post->updated_at->setTimezone('America/Chicago')->format(Post::DATE_FORMAT) }}}</p>
+        <img class='img-responsive' src="http://placehold.it/900x300" alt="">
+        <p id='body'>{{{ $post->body }}}</p>
+        <a class="btn btn-sm btn-primary" href="posts/{{ $post->id }}">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+        @empty
+        <p>No mo post</p>
+        @endforelse
+    </article>
 
     <hr>
 
-<!-- Paginator -->
+    <!-- Paginator -->
     <ul class="pager">
         <li>
             {{ $posts->links() }}
