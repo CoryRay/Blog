81530af187ccc9d0a4dafa6037cca986e3d44b27
Cory Rodriguez <coryrodriguez@Corys-MacBook-Air.local>
2014-10-09 16:12:52 -0500

    created and extended basemodel

--- app/models/BaseModel.php
+++ app/models/BaseModel.php
@@ -1,8 +1,18 @@
 <?php
 
-use Carbon\Carbon
+use Carbon\Carbon;
 
 class BaseModel extends Eloquent
 {
-    
+    public function setCreatedAtAttribute($value)
+    {
+        $utc = Carbon::createFromFormat($this->getDateFormat(), $value);
+        return $utc->setTimezone('America/Chicago');
+    }
+
+    public function setUpdatedAtAttribute($value)
+    {
+        $utc = Carbon::createFromFormat($this->getDateFormat(), $value);
+        return $utc->setTimezone('America/Chicago');
+    }
 }
