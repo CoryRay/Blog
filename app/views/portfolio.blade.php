e2c2bb7ade7c433850d0b00384d61e2c82651590
Cory Rodriguez <coryrodriguez@Corys-MacBook-Air.local>
2014-10-10 13:30:44 -0500

    started working on portfolio page

--- app/views/portfolio.blade.php
+++ app/views/portfolio.blade.php
@@ -7,12 +7,9 @@
     <!-- Page Heading -->
     <div class="row">
         <div class="col-lg-12">
-            <h1 class="page-header">Page Heading
-                <small>Secondary Text</small>
-            </h1>
+            <h1 class="page-header">Some of my Projects<small></small></h1>
         </div>
-    </div>
-    <!-- /.row -->
+    </div> <!-- /.row -->
 
     <!-- Project One -->
     <div class="row">
@@ -22,14 +19,12 @@
             </a>
         </div>
         <div class="col-md-5">
-            <h3>Project One</h3>
-            <h4>Subheading</h4>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium veniam exercitationem expedita laborum at voluptate. Labore, voluptates totam at aut nemo deserunt rem magni pariatur quos perspiciatis atque eveniet unde.</p>
+            <h3>Todo List</h3>
+            <h4>CLI App/Webapp</h4>
+            <p>A simple todo list app made in Codeup. It started out as a command line app and later on evolved into a nice web application.</p>
             <a class="btn btn-primary" href="#">View Project <span class="glyphicon glyphicon-chevron-right"></span></a>
         </div>
-    </div>
-    <!-- /.row -->
-
+    </div> <!-- /.row -->
     <hr>
 
     <!-- Project Two -->
@@ -40,14 +35,12 @@
             </a>
         </div>
         <div class="col-md-5">
-            <h3>Project Two</h3>
+            <h3>Address Book</h3>
             <h4>Subheading</h4>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, odit velit cumque vero doloremque repellendus distinctio maiores rem expedita a nam vitae modi quidem similique ducimus! Velit, esse totam tempore.</p>
             <a class="btn btn-primary" href="#">View Project <span class="glyphicon glyphicon-chevron-right"></span></a>
         </div>
-    </div>
-    <!-- /.row -->
-
+    </div> <!-- /.row -->
     <hr>
 
     <!-- Project Three -->
@@ -58,14 +51,12 @@
             </a>
         </div>
         <div class="col-md-5">
-            <h3>Project Three</h3>
-            <h4>Subheading</h4>
+            <h3>Whack a Mole</h3>
+            <h4>jQuery Web Game</h4>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis, temporibus, dolores, at, praesentium ut unde repudiandae voluptatum sit ab debitis suscipit fugiat natus velit excepturi amet commodi deleniti alias possimus!</p>
             <a class="btn btn-primary" href="#">View Project <span class="glyphicon glyphicon-chevron-right"></span></a>
         </div>
-    </div>
-    <!-- /.row -->
-
+    </div> <!-- /.row -->
     <hr>
 
     <!-- Project Four -->
@@ -82,9 +73,7 @@
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo, quidem, consectetur, officia rem officiis illum aliquam perspiciatis aspernatur quod modi hic nemo qui soluta aut eius fugit quam in suscipit?</p>
             <a class="btn btn-primary" href="#">View Project <span class="glyphicon glyphicon-chevron-right"></span></a>
         </div>
-    </div>
-    <!-- /.row -->
-
+    </div> <!-- /.row -->
     <hr>
 
     <!-- Project Five -->
@@ -100,52 +89,7 @@
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, quo, minima, inventore voluptatum saepe quos nostrum provident ex quisquam hic odio repellendus atque porro distinctio quae id laboriosam facilis dolorum.</p>
             <a class="btn btn-primary" href="#">View Project <span class="glyphicon glyphicon-chevron-right"></span></a>
         </div>
-    </div>
-    <!-- /.row -->
-
+    </div> <!-- /.row -->
     <hr>
-
-    <!-- Pagination -->
-    <div class="row text-center">
-        <div class="col-lg-12">
-            <ul class="pagination">
-                <li>
-                    <a href="#">&laquo;</a>
-                </li>
-                <li class="active">
-                    <a href="#">1</a>
-                </li>
-                <li>
-                    <a href="#">2</a>
-                </li>
-                <li>
-                    <a href="#">3</a>
-                </li>
-                <li>
-                    <a href="#">4</a>
-                </li>
-                <li>
-                    <a href="#">5</a>
-                </li>
-                <li>
-                    <a href="#">&raquo;</a>
-                </li>
-            </ul>
-        </div>
-    </div>
-    <!-- /.row -->
-
-    <hr>
-
-    <!-- Footer -->
-    <footer>
-        <div class="row">
-            <div class="col-lg-12">
-                <p>Copyright &copy; Your Website 2014</p>
-            </div>
-        </div>
-        <!-- /.row -->
-    </footer>
-
 </div>
 @stop
