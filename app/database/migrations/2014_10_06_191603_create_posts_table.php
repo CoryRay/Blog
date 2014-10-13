edfaf2a87d39f4bd2ca6770eca06c961e122384f
Cory Rodriguez <coryrodriguez@Corys-MacBook-Air.local>
2014-10-13 10:53:50 -0500

    cleanup

--- app/database/migrations/2014_10_06_191603_create_posts_table.php
+++ app/database/migrations/2014_10_06_191603_create_posts_table.php
@@ -5,31 +5,30 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreatePostsTable extends Migration {
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::create('posts', function(Blueprint $table)
-		{
-			$table->increments('id');
-			$table->string('title', 255);
-			$table->text('body');
-			$table->string('img', 255);
-			$table->timestamps();
-		});
-	}
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function(Blueprint $table)
+        {
+            $table->increments('id');
+            $table->string('title', 255);
+            $table->text('body');
+            $table->string('img', 255);
+            $table->timestamps();
+        });
+    }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::drop('posts');
-	}
-
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('posts');
+    }
 }
