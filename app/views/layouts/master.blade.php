1c58fee1b162e26e7bb47abac18e6c8e9941eaa1
Cory Rodriguez <coreyreylp@gmail.com>
2014-10-15 11:42:46 -0500

    added something nice

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <!-- Original Template Courtesy of Start Bootstrap -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     @yield('title')
