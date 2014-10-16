8857355be79bd56cd3d14d24c5f075cc94ef8ad7
Cory Rodriguez <coreyreylp@gmail.com>
2014-10-15 21:36:22 -0500

    rearranged routes and added routes for user editing

--- app/routes.php
+++ app/routes.php
@@ -5,8 +5,18 @@ Route::get('/', function()
     return View::make('hello');
 });
 
+Route::resource('posts', 'PostsController');
+
 Route::get('/resume', 'HomeController@showResume');
 Route::get('/portfolio', 'HomeController@showPortfolio');
+
+Route::get('/login', 'HomeController@showLogin');
+Route::post('/login', 'HomeController@doLogin');
+
+Route::get('/logout', 'HomeController@doLogout');
+
+Route::get('/user', 'HomeController@showUser');
+Route::post('/user', 'HomeController@editUser');
 
 Route::get('/rolldice/{guess}', function($guess)
 {
@@ -19,9 +29,3 @@ Route::get('orm-test', function ()
     $posts = Post::all();
     return var_dump($posts);
 });
-
-Route::resource('posts', 'PostsController');
-
-Route::get('/login', 'HomeController@showLogin');
-Route::post('/login', 'HomeController@doLogin');
-Route::get('/logout', 'HomeController@doLogout');
